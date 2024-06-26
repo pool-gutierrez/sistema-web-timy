a2a2b95e6ccf0f219410be21ac240c1b3c4fe74a
Pool Gutierrez <84854409+pool-gutierrez@users.noreply.github.com>
2024-06-26 17:40:08 -0500

    Proyecto iniciado

---
+++ src/main/java/com/pe/timy/SistemaWebTimyApplication.java
@@ -0,0 +1,13 @@
+package com.pe.timy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaWebTimyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaWebTimyApplication.class, args);
+	}
+
+}
